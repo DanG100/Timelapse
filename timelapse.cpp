2ca8d9f355de6265e63811faf1c27dfab58e751c
DanG100 <daniel.d.j.grau@gmail.com>
2014-09-05 17:42:32 -0800

    added right file this time

--- timelapse.cpp
+++ timelapse.cpp
@@ -7,7 +7,9 @@
 #include <string.h>
 #include <iostream>
 #include <fstream>
-#include <time.h>
+#include <ctime>
+#include <sstream>
+#include <string>
 #include <unistd.h>
 
 using namespace cv;
@@ -20,35 +22,41 @@ void updateImages();
 void editLog();
 
 #define THRESHOLD 15
-
+#define DEBUG printf("LINE: %d\n",__LINE__);
 Mat newImage,  HSV_oldImage, HSV_procImage, oldImage;
-
+String s;
 int runCounter = 0;
-int compareStatus, compareValue;
+float compareValue;
+bool imageSaved;
 
 VideoCapture cap;
 
 int main() {
 	//SETUP THE IMAGE AND CAMERA
 	char inputKey = waitKey(1);
-	cap.set(CV_CAP_PROP_FRAME_WIDTH, 640);
-	cap.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
+	cap.set(CV_CAP_PROP_FRAME_WIDTH, 1920);
+	cap.set(CV_CAP_PROP_FRAME_HEIGHT, 1080);
+	cap.open(0);
 	while(1){
-
-		cap.open(0);
+		DEBUG
+		
+		DEBUG
 		cap.grab();
+		DEBUG
 		getImage();
+		DEBUG
 		updateImages();
 		if (compareImage()){
 			saveImage();
 		}
 		inputKey = waitKey(1);
-
 		editLog();
 		runCounter++;
-		sleep(30);
+		sleep(10);
+
 	}
-	return (1);
+	cap.release();
+	return 1;
 }
 void getImage(){
 	cap >> newImage;
@@ -56,10 +64,10 @@ void getImage(){
 int compareImage(){
 	int h_bins = 50; int s_bins = 60;
 	int histSize[] = { h_bins, s_bins };
-	float h_ranges[] = { 0, 256 };
-	float s_ranges[] = { 0, 180 };
+	float h_ranges[] = { 0,180};
+	float s_ranges[] = { 0, 256};
 	const float* ranges[] = { h_ranges, s_ranges };
-	int channels[] = { 0};
+	int channels[] = {0,1};
 
 	MatND oldImage, procImage;
 
@@ -68,26 +76,37 @@ int compareImage(){
 
 	calcHist( &HSV_procImage, 1, channels, Mat(), procImage, 2, histSize, ranges, true, false );
 	normalize( procImage, procImage, 0, 1, NORM_MINMAX, -1, Mat() );
-
 	double oldImage_procImageCorrel = compareHist( oldImage, procImage, CV_COMP_CORREL);
-	double oldImage_procImageChisqr = compareHist( oldImage, procImage, CV_COMP_CHISQR);
-	double oldImage_procImageInter = compareHist( oldImage, procImage, CV_COMP_INTERSECT);
-	double oldImage_procImageHelli = compareHist( oldImage, procImage, CV_COMP_HELLINGER);
-	int compareValue = (oldImage_procImageCorrel + oldImage_procImageChisqr + oldImage_procImageInter + oldImage_procImageHelli)/4;
-	if( compareValue < THRESHOLD){
+	cvtColor(newImage, HSV_oldImage, CV_BGR2HSV);
+	compareValue = oldImage_procImageCorrel;
+	if( oldImage_procImageCorrel < 0.99){
+		imageSaved = true;
+		printf("Diff & compare value: %f\n", oldImage_procImageCorrel);
 		return 1;
+
 	}
 	else{
+		printf("Same & compare value: %f\n", oldImage_procImageCorrel);
+		imageSaved = false;
 		return 0;
+
 	}
 
 }
 void saveImage(){
-	 imwrite("/timelapse/image%d",runCounter, newImage);
-	 cvtColor(newImage, HSV_oldImage, CV_BGR2HSV);
+
+	stringstream ss;
+	ss << "images/image" << runCounter << ".png";
+	s = ss.str();
+   	 vector<int> compression_params;
+   	 compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
+    compression_params.push_back(1);
+
+	imwrite(s, newImage, compression_params);
+
 }
 void updateImages(){
-	if(runCounter!=0){
+	if(runCounter==0){
 		cvtColor(newImage, HSV_procImage, CV_BGR2HSV );
 		cvtColor(newImage, HSV_oldImage, CV_BGR2HSV );
 	}
@@ -101,10 +120,11 @@ void editLog(){
 	struct tm * timeinfo;
 	time ( &rawtime );
 	timeinfo = localtime ( &rawtime );
-	myfile.open("/timelapse/log.csv", ios::app | ios::out | ios::binary);
-	myfile << "image%d,",runCounter;
+	myfile.open("images/log.csv", ios::app | ios::out | ios::binary);
+	myfile << "Run Counter: "<< runCounter << ",";
+	myfile << "Compare: " << compareValue << ",";
+	myfile << "Image is Saved: " << imageSaved << ",";
 	myfile << asctime(timeinfo);
-	myfile << ",Compare: %d \n",compareValue;
 	myfile.close();
 }
 
